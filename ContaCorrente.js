80561bebafe74b3fbaf58d65ffef4c29aa150d41
Daniele <danikmaraschin@gmail.com>
2020-07-15 08:33:00 +0200

    working with setters, getters, null and undefined

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -1,13 +1,31 @@
+import {Cliente} from "./Cliente.js";
+
 export class ContaCorrente{
     agencia;
-    cliente;
+    
+    
+    
+    _cliente;
 
+    set cliente (novoValor){
+        if (novoValor instanceof Cliente){
+            this._cliente = novoValor;
+        }
+        
+    }
 
+        get cliente (){
+            return.this_cliente;
+        }
 //coloca _  na frente da palavra para especificar que é uma classe privada;
 //o dev nem deveria ter acesso e nao eh pra ele mexer nessa var.
 //estao tentando com a tralha na frente ser privado, mas ainda nao está oficial.
 
     _saldo = 0;
+
+    get saldo (){
+        return this._saldo; 
+    }
 
     sacar(valor){
         if(this._saldo >= valor){
--- index.js
+++ index.js
@@ -6,22 +6,19 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
+
 
 const ContaCorrenteRicardo = new ContaCorrente();
 ContaCorrenteRicardo.agencia = 1001;
 ContaCorrenteRicardo.cliente = cliente1;
 ContaCorrenteRicardo.depositar(500);
 
-
+const cliente2 = new Cliente();
 const conta2 =new ContaCorrente();
-conta2.cliente = cliente2;
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
 conta2.agencia = 102;
 
 let valor = 200;
 ContaCorrenteRicardo.transferir(valor,conta2);
-
 console.log(conta2);
-console.log("valor: ", valor);
