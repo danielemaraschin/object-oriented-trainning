d1e4a07783823ceddd3bb38a75f053040a0e8958
Daniele <danikmaraschin@gmail.com>
2020-07-22 18:58:49 +0200

    Learning inheritance,"super"and protected property

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-import {Cliente} from "./Cliente.js"
-import {ContaCorrente} from "./ContaCorrente.js"
-import {ContaPoupanca} from "./contaPoupanca.js"
-import {Conta} from "./conta.js"
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
+import { ContaPoupanca } from "./ContaPoupanca.js"
 
-const cliente1 = new Cliente("Ricardo", 11122233309 );
+const cliente1 = new Cliente("Ricardo", 11122233309);
 
-const ContaCorrenteRicardo = new ContaCorrente(cliente1, 1001 );
-ContaCorrenteRicardo.depositar(500);
-ContaCorrenteRicardo.sacar(100);
+const contaCorrenteRicardo = new ContaCorrente(cliente1, 1001);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
 
-const ContaPopanca = new ContaPoupanca (50, cliente1, 1001);
 
-console.log(ContaCorrenteRicardo);
-console.log(ContaPoupanca);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+contaPoupanca.sacar(10);
+
+console.log(contaCorrenteRicardo);
+console.log(contaPoupanca);
